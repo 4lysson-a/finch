e8c84fdd60e8406280b169daba29cf47ad50320d
4lysson-a <4ly.alcantara@gmail.com>
2024-07-28 01:44:26 -0300

    chore(backend): Remove unused import statements in PrismaService

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return createUserDto;
+  constructor(private readonly prismaService: PrismaService) { }
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.prismaService.user.create({
+      data: {
+        name: createUserDto.name,
+        email: createUserDto.email,
+        password: createUserDto.password,
+      },
+    });
+
+    return user;
   }
 
   findOne(id: number) {
