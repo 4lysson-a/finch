570bad8dd9985f5b29647c74ead4fc67fd83d486
4lysson-a <4ly.alcantara@gmail.com>
2024-09-26 14:26:54 -0300

    refactor(backend): Update .env.dist to add PORT variable

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalGuards();
 
-  await app.listen(3000);
+  await app.listen(Number(process.env.PORT || 3000));
 }
 
 bootstrap();
